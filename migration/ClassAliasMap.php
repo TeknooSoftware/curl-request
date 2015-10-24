26d2c41c298303ddf4dcb7f423c87b9a2b5c4487
Richard Deloge <r.deloge@uni-alteri.com>
2015-10-24 22:59:56 +0200

    Migrate to Teknoo Software organization

---
+++ migration/ClassAliasMap.php
@@ -0,0 +1,9 @@
+<?php
+return array(
+    'UniAlteri\Curl\ErrorException' => 'Teknoo\Curl\ErrorException',
+    'UniAlteri\Curl\Options' => 'Teknoo\Curl\Options',
+    'UniAlteri\Curl\OptionsInterface' => 'Teknoo\Curl\OptionsInterface',
+    'UniAlteri\Curl\Request' => 'Teknoo\Curl\Request',
+    'UniAlteri\Curl\RequestGenerator' => 'Teknoo\Curl\RequestGenerator',
+    'UniAlteri\Curl\RequestInterface' => 'Teknoo\Curl\RequestInterface',
+);
